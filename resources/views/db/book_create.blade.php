fd732212547becf6c1caf48f437b93d8073240ea
Testsuhiro <148023639+Testsuhiro@users.noreply.github.com>
2024-01-09 15:35:24 +0900

    Update book_create.blade.php

--- resources/views/db/book_create.blade.php
+++ resources/views/db/book_create.blade.php
@@ -6,6 +6,14 @@
     <title>書籍の登録</title>
 </head>
 <body>
-    
+    <h1>レビューの新規投稿</h1>
+    <form action="db/book_create" method="post"></form>
+    @csrf
+    社員名<br>
+    <input type="text" name="user_name" id="user_name" class="form-control" required>
+    <br>
+    レビュー<br>
+    <textarea name="review" id="review" class="form-control" rows="3" required></textarea>
+
 </body>
 </html>
