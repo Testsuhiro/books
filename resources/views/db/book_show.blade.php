2fcd2e3c7e5cff5b1e323113fa1b701c6951e244
Testsuhiro <148023639+Testsuhiro@users.noreply.github.com>
2024-01-09 15:34:57 +0900

    Update book_show.blade.php

--- resources/views/db/book_show.blade.php
+++ resources/views/db/book_show.blade.php
@@ -6,6 +6,26 @@
     <title>書籍の一覧表示</title>
 </head>
 <body>
-    
+    <!-- foreach追加 -->
+    <h1>書籍一覧</h1>
+    <table  border="1" class="table">
+        <thead>
+            <th>ISBN</th>
+            <th>書籍名</th>
+            <th>著者名</th>
+            <th>出版社名</th>
+            <th>価格</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1234567890987</td>
+                <td>スッキリ分かるjava</td>
+                <td>中山 清喬</td>
+                <td>インプレス</td>
+                <td>￥2860</td>
+            </tr>
+        </tbody>
+        </tr>
+    </table>
 </body>
 </html>
