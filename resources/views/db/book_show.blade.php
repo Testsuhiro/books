c94db4813e5dc9a8083ffd33042b079240848ff9
Testsuhiro <148023639+Testsuhiro@users.noreply.github.com>
2024-01-10 10:26:05 +0900

    Update book_show.blade.php

--- resources/views/db/book_show.blade.php
+++ resources/views/db/book_show.blade.php
@@ -10,22 +10,23 @@
     <h1>書籍一覧</h1>
     <table  border="1" class="table">
         <thead>
-            <th>ISBN</th>
-            <th>書籍名</th>
-            <th>著者名</th>
-            <th>出版社名</th>
-            <th>価格</th>
+            <th>ISBN</th>               <!--ISBN -->
+            <th>書籍名</th>             <!--書籍名 -->
+            <th>著者名</th>             <!--著者名 -->
+            <th>出版社名</th>           <!--出版社名 -->
+            <th>価格</th>               <!--価格 -->
         </thead>
         <tbody>
-            <tr>
-                <td>1234567890987</td>
-                <td>スッキリ分かるjava</td>
-                <td>中山 清喬</td>
-                <td>インプレス</td>
-                <td>￥2860</td>
-            </tr>
+            @foreach($records as $record)
+                <tr>
+                    <td></td>               <!--ISBN -->
+                    <td></td>               <!--書籍名 -->
+                    <td></td>               <!--著者名 -->
+                    <td></td>               <!--出版社名 -->
+                    <td></td>               <!--価格 -->
+                </tr>
+            @endforeach
         </tbody>
-        </tr>
     </table>
 </body>
 </html>
