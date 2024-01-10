741a0e1666fda9826b9378772c4a8ea8035aec80
Testsuhiro <148023639+Testsuhiro@users.noreply.github.com>
2024-01-10 10:26:58 +0900

    Update book_result.blade.php

--- resources/views/db/book_result.blade.php
+++ resources/views/db/book_result.blade.php
@@ -10,11 +10,11 @@
     <table class="table">
         <tr><th>ISBN</th><th>書籍名</th><th>著者名</th><th>出版社名</th><th>価格</th></tr>
         <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td>{{ $book_id }}</td>
+            <td>{{ $book_name }}</td>
+            <td>{{ $book_writer }}</td>
+            <td>{{ $book_pub }}</td>
+            <td>{{ $book_price }}</td>
         </tr>
         <br>
         <a href="/">Topページに戻る</a>
